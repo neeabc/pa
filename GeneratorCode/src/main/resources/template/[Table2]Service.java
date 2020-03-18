2356f6e1ccebf434d2d6f3c070cf3500bee2419e
xiao <603426335@qq.com>
2020-03-18 22:28:15 +0800

    代码生成器

---
+++ GeneratorCode/src/main/resources/template/[Table2]Service.java
@@ -0,0 +1,58 @@
+package [package].service;
+        [path_1].[path_2].pojo.Tb[Table2];
+/**
+ * [comment]服务层接口
+ * @author Administrator
+ *
+ */
+public interface [Table2]Service {
+
+	/**
+	 * 返回全部列表
+	 * @return
+	 */
+	public List<Tb[Table2]> findAll();
+	
+	
+	/**
+	 * 返回分页列表
+	 * @return
+	 */
+	public PageResult findPage(int pageNum,int pageSize);
+	
+	
+	/**
+	 * 增加
+	*/
+	public void add(Tb[Table2] [table2]);
+	
+	
+	/**
+	 * 修改
+	 */
+	public void update(Tb[Table2] [table2]);
+	
+
+	/**
+	 * 根据ID获取实体
+	 * @param id
+	 * @return
+	 */
+	public Tb[Table2] findOne([PrimaryKeyType] [PrimaryKeyName]);
+	
+	
+	/**
+	 * 批量删除
+	 * @param ids
+	 */
+	public void delete([PrimaryKeyType] [] [PrimaryKeyName]s);
+
+	/**
+	 * 分页
+	 * @param pageNum 当前页 码
+	 * @param pageSize 每页记录数
+	 * @return
+	 */
+	public PageResult findPage(Tb[Table2] [table2], int pageNum,int pageSize);
+	
+}
---
+++ GeneratorCode/src/main/resources/template/[Table2]ServiceImpl.java
@@ -0,0 +1,93 @@
+package import com.alibaba.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+[package].service.impl;
+        [path_1].[path_2].mapper.Tb[Table2]Mapper;
+        [path_1].[path_2].pojo.Tb[Table2];
+        [Table2]Example;
+        [Table2]Example.Criteria;
+        [package].service.[Table2]Service;
+
+/**
+ * [comment]服务实现层
+ * @author Administrator
+ *
+ */
+@Service
+public class [Table2]ServiceImpl implements [Table2]Service {
+
+	@Autowired
+	private Tb[Table2]Mapper [table2]Mapper;
+	
+	/**
+	 * 查询全部
+	 */
+	@Override
+	public List<Tb[Table2]> findAll() {
+		return [table2]Mapper.selectByExample(null);
+	}
+
+	/**
+	 * 按分页查询
+	 */
+	@Override
+	public PageResult findPage(int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);		
+		Page<Tb[Table2]> page=   (Page<Tb[Table2]>) [table2]Mapper.selectByExample(null);
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+
+	/**
+	 * 增加
+	 */
+	@Override
+	public void add(Tb[Table2] [table2]) {
+		[table2]Mapper.insert([table2]);		
+	}
+
+	
+	/**
+	 * 修改
+	 */
+	@Override
+	public void update(Tb[Table2] [table2]){
+		[table2]Mapper.updateByPrimaryKey([table2]);
+	}	
+	
+	/**
+	 * 根据ID获取实体
+	 * @param id
+	 * @return
+	 */
+	@Override
+	public Tb[Table2] findOne([PrimaryKeyType] [PrimaryKeyName]){
+		return [table2]Mapper.selectByPrimaryKey([PrimaryKeyName]);
+	}
+
+	/**
+	 * 批量删除
+	 */
+	@Override
+	public void delete([PrimaryKeyType][] [PrimaryKeyName]s) {
+		for([PrimaryKeyType] [PrimaryKeyName]:[PrimaryKeyName]s){
+			[table2]Mapper.deleteByPrimaryKey([PrimaryKeyName]);
+		}		
+	}
+	
+	
+		@Override
+	public PageResult findPage(Tb[Table2] [table2], int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		
+		Tb[Table2]Example example=new Tb[Table2]Example();
+		Criteria criteria = example.createCriteria();
+		
+		if([table2]!=null){			
+			[queryParms]	
+		}
+		
+		Page<Tb[Table2]> page= (Page<Tb[Table2]>)[table2]Mapper.selectByExample(example);		
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+	
+}
